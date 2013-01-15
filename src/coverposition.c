17a606f849b9cde1bc20ad1731ababb80cdae2ea
Helmuth Saatkamp <helmuthdu@gmail.com>
2013-01-15 16:23:50 -0200

    fixed cover position

--- src/coverposition.c
+++ src/coverposition.c
@@ -4,9 +4,9 @@
 //Cover Position
 void coverposition () {
 	if (cpu == 1)
-		yc += 126;
+		yc += 134;
 	else {
-		yc += 112;
+		yc += 116;
 		for (i = 1; i <= cpu; i++)
 		yc += 12;
 	}
@@ -17,7 +17,7 @@ void coverposition () {
 	if (set_battery == True)
 		yc += 14;
 	if (swap == True)
-		yc += 31;
+		yc += 26;
 	if (set_process == True) {
 		yc += 14;
 		for (i = True; i <= proc; i++)
@@ -25,7 +25,7 @@ void coverposition () {
 	}
 	if (nodata == False) {
 		if (clocktype == 1)
-			yc += 150;
+			yc += 58;
 			else
 				if (clocktype == 2)
 					yc += 60;
@@ -35,10 +35,10 @@ void coverposition () {
 			else
 				yc += 62;
 		if (set_calendar > 0)
-			yc += 70;
+			yc += 76;
 	}
 	if (set_photo == 1 || set_photo == 2) {
-		yc += 132;
+		yc += 124;
 	}
 	if (cover == 3 || cover == 6 || cover == 7)
 		yc -= 2;
